343c339291790583b13cf6e1b521443bf6739362
DRBATA <164792895+DRBATA@users.noreply.github.com>
2025-02-06 13:13:44 +0000

    basic PWA

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './app.css'
+import './App.css'
 
 // Import and register the service worker from the PWA plugin
 import { registerSW } from 'virtual:pwa-register'
